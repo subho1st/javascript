8bbfa977ec9fb9567552f216e33de908be0542f5
subho1st <rajdeepfirst@gmail.com>
2024-09-19 13:08:22 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+//let score = 34
+//let score = "34"
+//let score = "34abc" 
+//let score = null; // 0
+//let score = undefined;
+//let score = true;
+//let score = false;
+
+let score = "Raj";
+
+
+//console.log(typeof score);
+//console.log(typeof(score)); 
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+//let isLoggedIn = true;
+
+//let isLoggedIn = 1; // 1 => true, 0 => false
+//let isLoggedIn = ""; false
+let isLoggedIn = "Raj"; //true
+
+
+let booleanIsloggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsloggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
