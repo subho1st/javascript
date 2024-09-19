f1754413ad5c219218c3c469ba362c5c0eb541f5
subho1st <rajdeepfirst@gmail.com>
2024-09-19 12:32:16 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+// alert (3+3) // we are using node.js, not browser
+
+console.log(3+3);
+console.log("Raj");
+
+let name = "Raj"
+let age = 40
+
+
+//number => 2 to power 53
+//bigint //will use less
+//string => ""
+//boolean => true/function
+//null => standalone value
+//undefind
+//symbol => unique
+//object
+
+
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
