ebd854b38635d6de9bfd46c9b5f681a685930460
subho1st <rajdeepfirst@gmail.com>
2024-09-19 13:50:40 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -33,7 +33,54 @@ let booleanIsloggedIn = Boolean(isLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
 
 
+//***************** Operations *************
+
+let value = 3-8
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Raj";
+
+//console.log(str1+str2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(3 + 4 * 5 % 3);
+// console.log((3+4) * 5 % 3 );
+
+// console.log(true);
+// console.log(+true);// bad practice
+// console.log(+"");// bad practice
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 // bad practice 
+
+let gameCounter = 100;
+gameCounter++; //post increment
+//++gameCounter //pre increment
+//console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(x, y);
+
+let a = 3;
+const b = ++a;
+
+console.log(a, b);
