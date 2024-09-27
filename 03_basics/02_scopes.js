940b485644068af141b1194c0f0cd22c8f4fed7e
subho1st <rajdeepfirst@gmail.com>
2024-09-27 10:19:45 +0000

    mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -12,11 +12,53 @@ if (true){
     let a = 10
     const b = 20
     //var c = 30
-    console.log("Inner: ", a);
+    //console.log("Inner: ", a);
 }
 
 
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
+
+function one(){
+    const username = "Rajdeep";
+
+    function two(){
+        const website = "rajdeep.com";
+        console.log(username); 
+    }
+    //console.log(website);
+
+    two();
+    
+}
+
+//one();
+
+if (true) {
+    const username = "Subho";
+    if (username === "Subho") {
+        const website = " netflix.com"
+        console.log(username + website);
+    }
+}
+console.log(addOne(5));
+function addOne(num){
+    return num+1
+}
+
+//hoisting
+
+const addTwo = function(num){
+    return num+2
+}
+console.log(addTwo(6));
+
+
+
+
+
+
+
+
