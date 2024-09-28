07bf1b83b6dbc1b18a091681f33fc4e55eb9d506
subho1st <rajdeepfirst@gmail.com>
2024-09-28 06:02:23 +0000

    jsexecution

---
+++ 03_basics/05_jsexecutioncontext.js
@@ -0,0 +1,10 @@
+// JavaScript Execution Context
+
+// {} Global Execution Context
+
+// 1. Global Execution Context
+// 2. Function Execution Context
+// 3. Eval Execution Context
+
+// Memory Creation Phase
+// Execution Phase
